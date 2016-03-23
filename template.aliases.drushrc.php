fb769900a11edf790881237b7fca838168265927
Alex Harries <alex@greyhead.co.uk>
2016-03-23 14:59:11 +0000

    Re-jig the placeholders in the drush alias template.
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

--- template.aliases.drushrc.php
+++ template.aliases.drushrc.php
@@ -28,11 +28,11 @@ $aliases['local'] = array(
 
   // Absolute server path to the Drupal index.php file, without a trailing
   // slash, e.g. "/Volumes/Sites/4Com/drupal7/www".
-  'root' => '{{LOCALBUILDPATH}}',
+  'root' => '{{BUILDPATH-LOCAL}}',
 
   // The URL that your site will be hosted at, without the http(s):// or a
   // trailing slash.
-  'uri' => '{{LOCALSITEURI}}',
+  'uri' => '{{SITEURI-LOCAL}}',
 
   // Turning off strict checking is usually helpful on older servers; set this
   // to '0' (including the quotes) to turn off, or '1' to turn on.
@@ -58,18 +58,18 @@ $aliases['dev'] = array(
 
   // Absolute server path to the Drupal index.php file, without a trailing
   // slash, e.g. "/Volumes/Sites/4Com/drupal7/www".
-  'root' => '{{DEVBUILDPATH}}',
+  'root' => '{{BUILDPATH-DEV}}',
 
   // The remote host, which will be called by drush as an SSH command, so if you
   // write "ssh monkey@server.data.com -p 12345" to ssh into this server, you
   // will enter "server.data.com" here.
-  'remote-host' => '{{DEVREMOTEHOST}}',
+  'remote-host' => '{{REMOTEHOST-DEV}}',
 
   // This is the username you use to SSH into the server - this user needs to
   // be able to access the Drupal webroot (and make changes to the Drupal files,
   // if you want it to be able to do things like update modules, or do a git
   // pull). In the example above, remote-user is "monkey".
-  'remote-user' => '{{DEVREMOTEUSER}}',
+  'remote-user' => '{{REMOTEUSER-DEV}}',
 
   // Any other SSH options you need should be added here; in the example above,
   // we specify a different port to connect via - "-p 2020", so we add that
@@ -78,7 +78,7 @@ $aliases['dev'] = array(
 
   // The URL that your site will be hosted at, without the http(s):// or a
   // trailing slash.
-  'uri' => '{{DEVSITEURI}}',
+  'uri' => '{{SITEURI-DEV}}',
 
   // Turning off strict checking is usually helpful on older servers; set this
   // to '0' (including the quotes) to turn off, or '1' to turn on.
@@ -95,7 +95,7 @@ $aliases['staging'] = array(
   // This is the site alias you will use at the command line, for example if you
   // are going to do "drush @monkey-media.staging cc all", this should be
   // "monkey-media".
-  'site' => '{{STAGINGMULTISITENAMENOHYPHENS}}',
+  'site' => '{{MULTISITENAMENOHYPHENS}}',
 
   // This is the variant of the site; e.g. in the above example, where we wrote
   // "drush @monkey-media.staging cc all", the variant is "staging". Typically, the
@@ -104,18 +104,18 @@ $aliases['staging'] = array(
 
   // Absolute server path to the Drupal index.php file, without a trailing
   // slash, e.g. "/Volumes/Sites/4Com/drupal7/www".
-  'root' => '{{STAGINGBUILDPATH}}',
+  'root' => '{{BUILDPATH-STAGING}}',
 
   // The remote host, which will be called by drush as an SSH command, so if you
   // write "ssh monkey@server.data.com -p 12345" to ssh into this server, you
   // will enter "server.data.com" here.
-  'remote-host' => '{{STAGINGREMOTEHOST}}',
+  'remote-host' => '{{REMOTEHOST-STAGING}}',
 
   // This is the username you use to SSH into the server - this user needs to
   // be able to access the Drupal webroot (and make changes to the Drupal files,
   // if you want it to be able to do things like update modules, or do a git
   // pull). In the example above, remote-user is "monkey".
-  'remote-user' => '{{STAGINGREMOTEUSER}}',
+  'remote-user' => '{{REMOTEUSER-STAGING}}',
 
   // Any other SSH options you need should be added here; in the example above,
   // we specify a different port to connect via - "-p 2020", so we add that
@@ -124,7 +124,7 @@ $aliases['staging'] = array(
 
   // The URL that your site will be hosted at, without the http(s):// or a
   // trailing slash.
-  'uri' => '{{STAGINGSITEURI}}',
+  'uri' => '{{SITEURI-STAGING}}',
 
   // Turning off strict checking is usually helpful on older servers; set this
   // to '0' (including the quotes) to turn off, or '1' to turn on.
@@ -141,7 +141,7 @@ $aliases['live'] = array(
   // This is the site alias you will use at the command line, for example if you
   // are going to do "drush @monkey-media.live cc all", this should be
   // "monkey-media".
-  'site' => '{{LIVEMULTISITENAMENOHYPHENS}}',
+  'site' => '{{MULTISITENAMENOHYPHENS}}',
 
   // This is the variant of the site; e.g. in the above example, where we wrote
   // "drush @monkey-media.staging cc all", the variant is "staging". Typically, the
@@ -150,27 +150,27 @@ $aliases['live'] = array(
 
   // Absolute server path to the Drupal index.php file, without a trailing
   // slash, e.g. "/Volumes/Sites/4Com/drupal7/www".
-  'root' => '{{LIVEBUILDPATH}}',
+  'root' => '{{BUILDPATH-LIVE}}',
 
   // The remote host, which will be called by drush as an SSH command, so if you
   // write "ssh monkey@server.data.com -p 12345" to ssh into this server, you
   // will enter "server.data.com" here.
-  'remote-host' => '{{LIVEREMOTEHOST}}',
+  'remote-host' => '{{REMOTEHOST-LIVE}}',
 
   // This is the username you use to SSH into the server - this user needs to
   // be able to access the Drupal webroot (and make changes to the Drupal files,
   // if you want it to be able to do things like update modules, or do a git
   // pull). In the example above, remote-user is "monkey".
-  'remote-user' => '{{LIVEREMOTEUSER}}',
+  'remote-user' => '{{REMOTEUSER-LIVE}}',
 
   // Any other SSH options you need should be added here; in the example above,
   // we specify a different port to connect via - "-p 2020", so we add that
   // here.
-  'ssh-options' => '{{LIVESSHOPTIONS}}',
+  'ssh-options' => '{{SSHOPTIONS-LIVE}}',
 
   // The URL that your site will be hosted at, without the http(s):// or a
   // trailing slash.
-  'uri' => '{{LIVESITEURI}}',
+  'uri' => '{{SITEURI-LIVE}}',
 
   // Turning off strict checking is usually helpful on older servers; set this
   // to '0' (including the quotes) to turn off, or '1' to turn on.
